d8104311f38812f00e789d230eeb7f3d1a7a7372
sBujowski <s.bujowski@gmail.com>
2022-10-18 20:15:54 +0200

    small refactor yahooData.py, add todos, add __main__, add exemplary data file

--- data/external/yahooData.py
+++ data/external/yahooData.py
@@ -5,17 +5,17 @@ import yahoo_fin.stock_info as si
 from datetime import datetime
 
 
-def getSP500List():
-    '''
+def get_sp500_list():
+    """
     Return a Python list containing all the tickers from SP500
     If include_company_data is set to True, the tickers, company names, and sector information is returned as a data frame.
-    '''
+    """
     tickers = si.tickers_sp500(include_company_data=True)
     return tickers
 
 
-def getTickerData(ticker:str,interval:str,start:datetime,end:datetime):
-    '''
+def get_ticker_data(ticker: str, interval: str, start: datetime, end: datetime):
+    """
     Ticker: Company symbol for example: MSFT for microsoft
     period: duration of historic data
     interval: time interval of data
@@ -24,9 +24,20 @@ def getTickerData(ticker:str,interval:str,start:datetime,end:datetime):
     NOTE: There are some restriction in data
     For 1 min time interval : 1 week
     5 min : 30 days
-    1d: 10 years 
+    1d: 10 years
     We will be using 1d to train the model and may use 5 min to train the model
-    '''
-    df = yf.download(ticker=ticker,start=start,end=end,interval=interval).reset_index()
+    """
+    # todo - documentation
+    # todo - restriction handling
+    # todo - default params (?)
+    df = yf.download(tickers=ticker, start=start, end=end, interval=interval).reset_index()
     return df
 
+
+if __name__ == '__main__':
+    get_ticker_data(
+        ticker='AMD',
+        interval='1d',
+        start=datetime(2022, 7, 1),
+        end=datetime(2022, 7, 25)
+    ).to_csv('./exemplary-external.csv')
